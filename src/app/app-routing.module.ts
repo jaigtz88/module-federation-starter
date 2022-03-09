6a8e64b837d81ee0c3ea35ed4d3b63f749c9b998
aznanmoh <mohdfaizal.aznan@infineon.com>
2022-03-09 16:05:45 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FileType, MfeUtil } from 'utils';
+
+export const mef = new MfeUtil();
 
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'restaurants',
     loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomeModule),
+      mef
+        .loadRemoteFile({
+          remoteName: 'restaurant',
+          remoteEntry: `http://localhost:4204/remoteRestaurant.js`,
+          exposedFile: 'RestaurantModule',
+          exposeFileType: FileType.Module,
+        })
+        .then((m: { RestaurantModule: any }) => m.RestaurantModule),
+  },
+  {
+    path: 'order',
+    loadChildren: () =>
+      mef
+        .loadRemoteFile({
+          remoteName: 'orders',
+          remoteEntry: `http://localhost:4205/remoteOrders.js`,
+          exposedFile: 'OrderModule',
+          exposeFileType: FileType.Module,
+        })
+        .then((m: { OrderModule: any }) => m.OrderModule),
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CoreModule} from "./core/core.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import { CoreModule } from './core/core.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UtilsModule } from 'utils';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    CoreModule
+    CoreModule,
+    UtilsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
