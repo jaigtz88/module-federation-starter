b4f439a7067147d10fad6397b426ec094f10f72f
aznanmoh <mohdfaizal.aznan@infineon.com>
2022-03-09 12:38:06 +0800

    add changes

--- projects/app2-restaurant/src/app/app-routing.module.ts
+++ projects/app2-restaurant/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'restaurants',
+    loadChildren: () =>
+      import('./restaurant/restaurant.module').then((m) => m.RestaurantModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
