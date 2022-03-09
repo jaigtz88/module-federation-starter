11f47c4e20c90080ed5099de9d6e204e593ecc30
aznanmoh <mohdfaizal.aznan@infineon.com>
2022-03-09 12:55:43 +0800

    update

---
+++ projects/utils/src/lib/utils.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { UtilsComponent } from './utils.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [UtilsComponent],
+  imports: [RouterModule],
+  exports: [UtilsComponent],
+})
+export class UtilsModule {}
